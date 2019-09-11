0e92d9f2651470f3fad2f669739257ef31e6ace9
drewjel <andrew.james.elsey@gmail.com>
2019-09-11 07:26:48 -0400

    semper amemus

---
+++ khietworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace khietworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
